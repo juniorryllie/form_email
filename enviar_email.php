8c246de823260eecd5a273a35ada9cbd6bb200fa
unknown <adm@.(none)>
2010-03-11 20:51:40 -0300

    Segunda versao

---
+++ enviar_email.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Untitled Page</title>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style> article, aside, figure, footer, header, hgroup, menu, nav, section { display: block; } </style>
+	
+</head>
+<body>
+	<div class="container">
+		
+	</div>
+</body>
+</html>
